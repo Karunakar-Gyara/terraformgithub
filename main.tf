576115c5f9a545c1de5cbf953619f62e930a75a8
karunakar <karunakargyara74@gmail.com>
2024-04-26 22:24:24 +0530

    added the backend

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "static" {
-  name          = "githubterraformbucket-123"
+  name          = "githubterraformbucket-1234"
   location      = "US"
   project       = "precise-bonus-421514"
   storage_class = "STANDARD"
