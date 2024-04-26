51024364d6136def1411518b9766caadcca79642
karunakar <karunakargyara74@gmail.com>
2024-04-26 20:59:56 +0530

    a

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "google_storage_bucket" "static" {
- name          = "githubterraformbucket"
- location      = "US"
- storage_class = "STANDARD"
+  name          = "githubterraformbucket"
+  location      = "US"
+  storage_class = "STANDARD"
 
- uniform_bucket_level_access = true 
- lifecycle {
-   prevent_destroy = true
- }
+  uniform_bucket_level_access = true
+  lifecycle {
+    prevent_destroy = true
+  }
 
-  
+
 }
