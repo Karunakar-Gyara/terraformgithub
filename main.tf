1529d31ba9e5d0e9de74f4f4ad933fb25a36f377
karunakar <karunakargyara74@gmail.com>
2024-04-26 21:07:16 +0530

    inialcode

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "static" {
   name          = "githubterraformbucket"
   location      = "US"
+  project       = "precise-bonus-421514"
   storage_class = "STANDARD"
 
   uniform_bucket_level_access = true
